ef82e0bcfb85342046b64f8242d970176381faec
Alex Land <aland@sfu.ca>
2016-03-02 20:00:25 -0800

    UNTESTED added bluetooth send/receive functionality

--- GunDisplay.ino
+++ GunDisplay.ino
@@ -3,8 +3,22 @@
 
   This displays the 2 7-segment display of the Halo gun ammo.
  */
+
+#include <SPI.h>
+#include "Adafruit_BLE_UART.h"
+
 #define HIGH 0x1
 #define LOW  0x0
+
+/******** BLUETOOTH *********
+ Connect CLK/MISO/MOSI to hardware SPI
+ e.g. On UNO & compatible: CLK = 13, MISO = 12, MOSI = 11
+*/
+#define ADAFRUITBLE_REQ 10
+#define ADAFRUITBLE_RDY 2     // This should be an interrupt pin, on Uno thats #2 or #3
+#define ADAFRUITBLE_RST 9
+
+Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
 
 const int MAX_AMMO = 30;
 int amountOfAmmo = MAX_AMMO;
@@ -17,20 +31,20 @@ int SPEED = 9; //How fast the numbers deincrement when in machine gun mode
 int timeLapse = 0; //A counter for the determing when to deincrement machine gun mode ammo
 
 //Pins of the display
-const int DIGIT_TOP_BAR = 2;
-const int DIGIT_MIDDLE_VERTICAL_BARS = 3;
-const int DIGIT_MIDDLE_HORIZONTAL_BARS = 4;
-const int DIGIT_UPPER_RIGHT_VERTICAL_BAR = 5;
-const int DIGIT_LOWER_RIGHT_VERTICAL_BAR = 6;
-const int DIGIT_LOWER_LEFT_VERTICAL_BAR = 7;
-const int DIGIT_UPPER_LEFT_VERTICAL_BAR = 8;
-const int DIGIT_BOTTOM_BAR = 9;
-const int BUTTON_PIN = 10;
-const int LEFT_NUMBER_DISPLAY = 11;
-const int RIGHT_NUMBER_DISPLAY = 12;
-const int SWITCH = 13;
+const int DIGIT_TOP_BAR = 2; //14
+const int DIGIT_MIDDLE_VERTICAL_BARS = 3; //15
+const int DIGIT_MIDDLE_HORIZONTAL_BARS = 4; //16
+const int DIGIT_UPPER_RIGHT_VERTICAL_BAR = 5; //17
+const int DIGIT_LOWER_RIGHT_VERTICAL_BAR = 6; //18
+const int DIGIT_LOWER_LEFT_VERTICAL_BAR = 7; //19
+const int DIGIT_UPPER_LEFT_VERTICAL_BAR = 8; //3
+const int DIGIT_BOTTOM_BAR = 9; //4
+const int BUTTON_PIN = 10; //5
+const int LEFT_NUMBER_DISPLAY = 11; //6
+const int RIGHT_NUMBER_DISPLAY = 12; //7
+const int SWITCH = 13; //8
 
-//turns on and off led bars 
+//turns on and off led bars
 void displayLEDBar(int pinNumber, boolean isOn) {
   if (isOn == true) {
     digitalWrite(pinNumber, HIGH);
@@ -132,6 +146,8 @@ void setup() {
   pinMode(SWITCH, INPUT_PULLUP);
 
   Serial.begin(9600);
+  while(!Serial); // Wait for serial init
+  BTLEserial.begin();
 }
 
 void testBarsAreWorking() {
@@ -242,6 +258,28 @@ void displayNumber(boolean isOn, int digit) {
   }
 }
 
+char * readBluetooth(Adafruit_BLE_UART BTLEserial) {
+  // Lets see if there's any data for us!
+  int bytesavailable = BTLEserial.available();
+  if (bytesavailable) {
+    Serial.print("* "); Serial.print(bytesavailable); Serial.println(F(" bytes available from BTLE"));
+  }
+
+  char * buffer = char[bytesavailable];
+
+  for (int i = 0; i < bytesavailable; i++) {
+    buffer[i] = BTLEserial.read();
+  }
+
+  return buffer;
+}
+
+void sendBluetooth(Adafruit_BLE_UART BTLEserial, char * sendbuffer, int sendbuffersize) {
+  /*Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");*/
+  // write the data
+  BTLEserial.write(sendbuffer, sendbuffersize);
+}
+
 // the loop function runs over and over again forever
 void loop() {
   //  testBarsAreWorking();
@@ -278,5 +316,16 @@ void loop() {
     showLeftNumber = true;
   }
   displayAmmo();
+
+  BTLEserial.pollACI();
+  aci_evt_opcode_t status = BTLEserial.getState();
+
+  if (status == ACI_EVT_CONNECTED) {
+    int strlength = 2;
+    char str[strlength];
+    sprintf(str, "%d", amountOfAmmo);
+    sendBluetooth(BTLEserial, str, strlength);
+  }
+
   delay(5);
 }
