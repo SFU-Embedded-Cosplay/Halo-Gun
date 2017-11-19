87ce64880ced1b2e5e2e29334d24baf7db655dfb
Rebecca Dun <rebeccadun@d192-075-241-237.wireless.sfu.ca>
2017-11-18 16:11:14 -0800

    Added Bluetooth Functionality for Genuino

--- GunDisplay/GunDisplay.ino
+++ GunDisplay/GunDisplay.ino
@@ -5,149 +5,76 @@
   This displays the 2 7-segment display of the Halo gun ammo.
  */
 
-#include <SPI.h>
-#include <Adafruit_BLE_UART.h>
-#include <Thread.h>
+#include <CurieBLE.h>
 #include "AmmoDisplay.h"
 
 #define HIGH 0x1
 #define LOW  0x0
 
-/******** BLUETOOTH *********
- Connect CLK/MISO/MOSI to hardware SPI
- e.g. On UNO & compatible: CLK = 13, MISO = 12, MOSI = 11
-*/
-#define ADAFRUITBLE_REQ 19
-#define ADAFRUITBLE_RDY 21     // This should be an interrupt pin, on Uno thats #2 or #3
-#define ADAFRUITBLE_RST 20
+BLEPeripheral blePeripheral; // create peripheral instance
 
-Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
-Thread btSendThread = Thread();
-Thread btReceiveThread = Thread();
-
-const char RELOAD_COMMAND[] = "Reload";
-const int RELOAD_COMMAND_LENGTH = 6;
-const char SINGLE_SHOT_COMMAND[] = "SingleShot";
-const int SINGLE_SHOT_COMMAND_LENGTH = 10;
-const char FULL_AUTO_COMMAND[] = "FullAuto";
-const int FULL_AUTO_COMMAND_LENGTH = 8;
+BLEService gunService("19B10000-E8F2-537E-4F6C-D104768A1214");
+// create switch characteristic and allow remote device to read and write
 const int MAX_AMMO = 30;
+BLEUnsignedIntCharacteristic ammo("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead);
+
+const int NO_COMMAND = 0;
+const int RELOAD_COMMAND = 1;
+// TODO: Add other commands for assault rifle (single shot and full auto)
+BLEUnsignedIntCharacteristic command("19B10002-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
 
 AmmoDisplay display(MAX_AMMO);
 
 int pastButtonState = 0;     // previous state of the button
-bool firingMode = false;
+bool firingMode = true;
 
 const int BUTTON_PIN = 41; //5
 const int SWITCH = 42; //8
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
-
   display.init();
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(SWITCH, INPUT_PULLUP);
 
   Serial.begin(9600);
   while(!Serial); // Wait for serial init
-  BTLEserial.begin();
-  btSendThread.onRun(bluetoothSendThreadMain);
-  // We will only run this thread when the ammo is decremented,
-  // so we don't want any other internal interfering
-  btSendThread.setInterval(0);
-  btReceiveThread.onRun(bluetoothReceiveThreadMain);
-  btReceiveThread.setInterval(100);
-}
 
-void updateBluetoothAmmo() {
-  if (btSendThread.shouldRun()) {
-    aci_evt_opcode_t status = BTLEserial.getState();
-    if (status == ACI_EVT_CONNECTED) {
-          btSendThread.run();
-    }
-  }
+  blePeripheral.setAdvertisedServiceUuid(gunService.uuid());
+  
+  blePeripheral.addAttribute(gunService);
+  blePeripheral.addAttribute(ammo);
+  blePeripheral.addAttribute(command);
+
+  // TODO: Change max ammo for pistol
+  ammo.setEventHandler(BLERead, updateAmmo);
+  ammo.setValue(MAX_AMMO);
+
+  command.setEventHandler(BLEWritten, handleCommand);
+  command.setValue(0);
+  
+  blePeripheral.begin();
 }
 
 void reload() {
   display.reload();
-  updateBluetoothAmmo();
 }
 
-char * readBluetooth(Adafruit_BLE_UART BTLEserial) {
-  // Lets see if there's any data for us!
-  int bytesavailable = BTLEserial.available();
-  if (bytesavailable) {
-    Serial.print("* "); Serial.print(bytesavailable); Serial.println(F(" bytes available from BTLE"));
-  }
-
-  char * buffer = (char *) calloc(bytesavailable, sizeof(char));
-  for (int i = 0; i < bytesavailable; i++) {
-    buffer[i] = BTLEserial.read();
-  }
-
-  return buffer;
+void updateAmmo(BLECentral& central, BLEUnsignedIntCharacteristic& ammo) {
+  ammo.setValue(display.getCurrentAmmo());
 }
 
-void sendBluetooth(Adafruit_BLE_UART BTLEserial, uint8_t * sendbuffer, char sendbuffersize) {
-  //Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");
-  BTLEserial.write(sendbuffer, sendbuffersize);
-}
-
-void sendAmmoOverBluetooth() {
-  char strlength = display.getCurrentAmmo() >= 10 ? 3 : 2;
-  char str[strlength];
-  sprintf(str, "%d", display.getCurrentAmmo());
-  sendBluetooth(BTLEserial, (uint8_t*)str, strlength);
-}
-
-bool checkForReloadCommand(char * message) {
-  int result = strncmp(message, RELOAD_COMMAND, RELOAD_COMMAND_LENGTH);
-  if (result == 0) {
-    Serial.println("Reload");
-    return true;
-  }
-  return false;
-}
-
-bool checkForSingleShotCommand(char * message) {
-  int result = strncmp(message, SINGLE_SHOT_COMMAND, SINGLE_SHOT_COMMAND_LENGTH);
-  if (result == 0) {
-    Serial.println("Single shot");
-    return true;
-  }
-  return false;
-}
-
-bool checkForFullAutoCommand(char * message) {
-  int result = strncmp(message, FULL_AUTO_COMMAND, FULL_AUTO_COMMAND_LENGTH);
-  if (result == 0) {
-    Serial.println("Full auto");
-    return true;
-  }
-  return false;
-}
-
-// Called every time btSendThread is ran
-void bluetoothSendThreadMain() {
-  sendAmmoOverBluetooth();
-}
-
-// Called every time btReceiveThread is ran
-void bluetoothReceiveThreadMain() {
-  if (BTLEserial.available() > 0) {
-    char * message = readBluetooth(BTLEserial);
-    if (checkForReloadCommand(message)) {
+void handleCommand(BLECentral& central, BLEUnsignedIntCharacteristic& command) {
+  // TODO: handle different firing modes by changing firingMode boolean
+  switch(command.value()) {
+    case NO_COMMAND:
+      break;
+    case RELOAD_COMMAND:
       reload();
-    }
-
-    if (checkForSingleShotCommand(message)) {
-      firingMode = true;
-    } else if (checkForFullAutoCommand(message)) {
-      firingMode = false;
-    }
+      break;
+    default:
+      break;
   }
-  
 }
 
 // delay for a given amount of time to avoid running on the main loop
@@ -178,49 +105,29 @@ void loop() {
     reload();
   }
 
-  // Check the BT connection for any incoming data
-  BTLEserial.pollACI();
-  if (btReceiveThread.shouldRun()) {
-    aci_evt_opcode_t status = BTLEserial.getState();
-    if (status == ACI_EVT_CONNECTED) {
-          btReceiveThread.run();
-    }
-  }
-
   display.turnOffDisplay();
   bool switchStatus = getFiringModeStatus();
   int buttonState = digitalRead(BUTTON_PIN);
-  bool sendBluetoothUpdate = false;
 
   // //Single Shot Display
   if (switchStatus) {
     if (buttonState != pastButtonState) {
       if (!buttonState == HIGH) {
         Serial.println(display.getCurrentAmmo());
-        bool ammoChanged = display.decrementAmmo();
-        if (ammoChanged) {
-          sendBluetoothUpdate = true;
-        }
+        display.decrementAmmo();
       }
     }
     pastButtonState = buttonState;
   } else {   //Machine Gun Display
     if (buttonState == LOW) {
       Serial.println(display.getCurrentAmmo());
-      bool ammoChanged = display.decrementAmmo();
-      if (ammoChanged) {
-        sendBluetoothUpdate = true;
-      }
+      display.decrementAmmo();
     }
   }
 
   //Alternate between the 2 7-segment displays
   display.changeDisplaySide();
   display.refresh();
-
-  if (sendBluetoothUpdate) {
-    updateBluetoothAmmo();
-  }
 
   delay(5);
 }
